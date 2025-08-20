d31c994e76dcb68238004136015e919c9f879228
Marina Adel <rinaa1775@gmail.com>
2025-08-21 00:08:26 +0300

    Initialize Taskati app

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Taskati());
+}
+
+class Taskati extends StatelessWidget {
+  const Taskati({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Scaffold());
+  }
+}
